80b0b405bc5468fd7a206f6dc6e1dd641e3e37fe
Esdidubs <sward3644@gmail.com>
2021-02-04 13:22:07 -0700

    cleaned up the code and removed unused portions

--- index.js
+++ index.js
@@ -1,62 +1,14 @@
-'use strict';
-
 // All global variables
 let STORE = [];
 let questionNumber = 1;
 let correctAnswers = 0;
 let incorrectAnswers = 0;
 let currentQuestion = 0;
-let correctMessage = `That is correct! Way to go!`;
-let incorrectMessage = `That is incorrect.`;
+let correctMessage;
+let incorrectMessage;
 
-// Handles the display of the scores
-function scores() {
-	const scoreMake = `
-        <li class="scores">Correct: ${correctAnswers}</li>
-        <li class="scores">Incorrect: ${incorrectAnswers}</li>
-    `;
-	return scoreMake;
-}
-
-// Sets up the form for each question
-function generateQuestion() {
-	const questionMake = `
-        <form>
-        <fieldset name="question">
-            <legend>Question ${questionNumber} of ${STORE.length}</legend>
-            ${STORE[currentQuestion].question}
-        </fieldset>
-        <fieldset name="answers">
-            <legend>Answers</legend>
-            <input type="radio" name="answer" class="posAnswer" value="${STORE[currentQuestion]
-				.answers[0]}" data-value= "${STORE[currentQuestion].answers[0]}"> ${STORE[currentQuestion]
-		.answers[0]}<br>
-            <input type="radio" name="answer" class="posAnswer" value="${STORE[currentQuestion]
-				.answers[1]}" data-value= "${STORE[currentQuestion].answers[1]}"> ${STORE[currentQuestion]
-		.answers[1]}<br>
-            <input type="radio" name="answer" class="posAnswer" value="${STORE[currentQuestion]
-				.answers[2]}" data-value= "${STORE[currentQuestion].answers[2]}"> ${STORE[currentQuestion]
-		.answers[2]}<br>
-            <input type="radio" name="answer" class="posAnswer" value="${STORE[currentQuestion]
-				.answers[3]}" data-value= "${STORE[currentQuestion].answers[3]}"> ${STORE[currentQuestion]
-		.answers[3]}<br>
-            <button name="answer" id="guess">Submit</button>
-            <button name="answer" id="next" class="hidden">Next</button>
-        </fieldset>
-        </form>
-        <p class="hidden" id="message">Test</p>
-        <p class="hidden wrong" id="error">"Please select an answer."</p>
-    `;
-
-	let encodedStr = STORE[currentQuestion].rightAnswer;
-	let parser = new DOMParser();
-	let dom = parser.parseFromString('<!doctype html><body>' + encodedStr, 'text/html');
-	let decodedString = dom.body.textContent;
-
-	correctMessage = `That is correct! Way to go!`;
-	incorrectMessage = `That is incorrect. The correct answer is: ${decodedString}.`;
-	return questionMake;
-}
+// Runs at start
+window.onload = function(){ buttons() };
 
 // All of the button press options
 function buttons() {
@@ -97,6 +49,7 @@ function buttons() {
 	});
 }
 
+// Uses fetch to utilize the API and sends the JSON data to the createArr function
 function createQuestions(url) {
 	fetch(url)
 		.then((response) => response.json())
@@ -104,12 +57,10 @@ function createQuestions(url) {
 		.catch((error) => alert(error));
 }
 
+
 function createArr(data) {
-	//console.log(data.results[0].incorrect_answers);
 	for (let question in data.results) {
-		//console.log(data.results[question].correct_answer);
 		let arr = [];
-		let shuffledArr = [];
 		arr.push(data.results[question].correct_answer);
 		arr.push(data.results[question].incorrect_answers[0]);
 		arr.push(data.results[question].incorrect_answers[1]);
@@ -128,16 +79,37 @@ function createArr(data) {
 	renderQuestion();
 }
 
-// Handles when someone presses the next button
-function handleNext() {
-	if (questionNumber != STORE.length) {
-		console.log('running the next question');
-		currentQuestion++;
-		questionNumber++;
-		renderQuestion();
-	} else {
-		$('.box').html(results());
-	}
+// Renders the question form
+function renderQuestion() {
+	$('.box').html(
+		`
+        <form>
+        <fieldset name="question">
+            <legend>Question ${questionNumber} of ${STORE.length}</legend>
+            ${STORE[currentQuestion].question}
+        </fieldset>
+        <fieldset name="answers">
+            <legend>Answers</legend>
+            <input type="radio" name="answer" class="posAnswer" value="${STORE[currentQuestion]
+				.answers[0]}" data-value= "${STORE[currentQuestion].answers[0]}"> ${STORE[currentQuestion]
+		.answers[0]}<br>
+            <input type="radio" name="answer" class="posAnswer" value="${STORE[currentQuestion]
+				.answers[1]}" data-value= "${STORE[currentQuestion].answers[1]}"> ${STORE[currentQuestion]
+		.answers[1]}<br>
+            <input type="radio" name="answer" class="posAnswer" value="${STORE[currentQuestion]
+				.answers[2]}" data-value= "${STORE[currentQuestion].answers[2]}"> ${STORE[currentQuestion]
+		.answers[2]}<br>
+            <input type="radio" name="answer" class="posAnswer" value="${STORE[currentQuestion]
+				.answers[3]}" data-value= "${STORE[currentQuestion].answers[3]}"> ${STORE[currentQuestion]
+		.answers[3]}<br>
+            <button name="answer" id="guess">Submit</button>
+            <button name="answer" id="next" class="hidden">Next</button>
+        </fieldset>
+        </form>
+        <p class="hidden" id="message">Test</p>
+        <p class="hidden wrong" id="error">"Please select an answer."</p>
+    `
+	);
 }
 
 // Handles when someone guesses an answer
@@ -153,24 +125,21 @@ function handleGuess() {
 	}
 }
 
-// Toggles the messages for correct/incorrect and shows the new score
-function toggleClasses() {
-	$('#message').toggleClass('hidden');
-	$('#guess').toggleClass('hidden');
-	$('#next').toggleClass('hidden');
-	$('.scoreboard').html(scores());
-}
-
 // Checks whether answer is correct or not
 function guess() {
 	if (!$('#error').hasClass('hidden')) {
 		$('#error').toggleClass('hidden');
 	}
 	$('input[type=radio]').attr('disabled', true);
+
+	// had to add this because there were issues with questions having apostrophes etc
 	let encodedStr = STORE[currentQuestion].rightAnswer;
 	let parser = new DOMParser();
 	let dom = parser.parseFromString('<!doctype html><body>' + encodedStr, 'text/html');
-	let decodedString = dom.body.textContent;
+	let decodedString = dom.body.textContent; 
+
+	correctMessage = `That is correct! Way to go!`;
+	incorrectMessage = `That is incorrect. The correct answer is: ${decodedString}.`; 
 
 	if ($("input[name='answer']:checked").data('value') == decodedString) {
 		$('#message').text(`${correctMessage}`);
@@ -182,28 +151,46 @@ function guess() {
 	toggleClasses();
 }
 
+// Toggles the messages for correct/incorrect and shows the new score
+function toggleClasses() {
+	$('#message').toggleClass('hidden');
+	$('#guess').toggleClass('hidden');
+	$('#next').toggleClass('hidden');
+	$('.scoreboard').html(scores());
+}
+
+
+// Handles when someone presses the next button
+function handleNext() {
+	if (questionNumber != STORE.length) {
+		currentQuestion++;
+		questionNumber++;
+		renderQuestion();
+	} else {
+		$('.box').html(results());
+	}
+}
+
 // This is the display for the results page.
 function results() {
 	let goodScore = 'You really know your trivia!';
 	let okayScore = 'Try again and see if you can do better!';
 	let badScore = 'This quiz was not meant for you.';
 	let scoreMessage = "If you're seeing this, there's an error.";
-
 	let resultImage = "If you're seeing this, there's an error.";
-
 	let resultImageAlt = 'Picture based on score';
 
 	if (correctAnswers > STORE.length * 0.7) {
 		scoreMessage = goodScore;
-		resultImage = 'images/thumbsup.png';
+		resultImage = 'images/party.png';
 		resultImageAlt = 'Celebrate';
 	} else if (correctAnswers > STORE.length * 0.4) {
 		scoreMessage = okayScore;
-		resultImage = 'images/restarting.png';
+		resultImage = 'images/thumbs-up.png';
 		resultImageAlt = 'Thumbs Up';
 	} else {
 		scoreMessage = badScore;
-		resultImage = 'images/slain.png';
+		resultImage = 'images/thumbs-down.png';
 		resultImageAlt = 'Thumbs Down';
 	}
 
@@ -246,31 +233,31 @@ function restartQuiz() {
             <br>
             <label for="category">Select a Category: </label>
             <select name="category" id="category">
-                <option value="any">Any Category</option>
-                <option value="9">General Knowledge</option>
-                <option value="10">Entertainment: Books</option>
-                <option value="11">Entertainment: Film</option>
-                <option value="12">Entertainment: Music</option>
-                <option value="13">Entertainment: Musicals &amp; Theatres</option>
-                <option value="14">Entertainment: Television</option>
-                <option value="15">Entertainment: Video Games</option>
-                <option value="16">Entertainment: Board Games</option>
-                <option value="17">Science &amp; Nature</option>
-                <option value="18">Science: Computers</option>
-                <option value="19">Science: Mathematics</option>
-                <option value="20">Mythology</option>
-                <option value="21">Sports</option>
-                <option value="22">Geography</option>
-                <option value="23">History</option>
-                <option value="24">Politics</option>
-                <option value="25">Art</option>
-                <option value="26">Celebrities</option>
-                <option value="27">Animals</option>
-                <option value="28">Vehicles</option>
-                <option value="29">Entertainment: Comics</option>
-                <option value="30">Science: Gadgets</option>
-                <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
-                <option value="32">Entertainment: Cartoon &amp; Animations</option>		
+				<option value="any">Any Category</option>
+				<option value="27">Animals</option>
+				<option value="25">Art</option>
+				<option value="16">Board Games</option>
+				<option value="10">Books</option>
+				<option value="32">Cartoon &amp; Animations</option>
+				<option value="26">Celebrities</option>
+				<option value="29">Comics</option>
+				<option value="11">Film</option>
+				<option value="9">General Knowledge</option>
+				<option value="22">Geography</option>
+				<option value="23">History</option>
+				<option value="31">Japanese Anime &amp; Manga</option>
+				<option value="12">Music</option>
+				<option value="13">Musicals &amp; Theatres</option>
+				<option value="20">Mythology</option>
+				<option value="24">Politics</option>
+				<option value="17">Science &amp; Nature</option>
+				<option value="18">Science: Computers</option>
+				<option value="30">Science: Gadgets</option>
+				<option value="19">Science: Mathematics</option>
+				<option value="21">Sports</option>
+				<option value="14">Television</option>
+				<option value="28">Vehicles</option>
+				<option value="15">Video Games</option>    
             </select>
             </fieldset>
             <button id="begin">Begin</button>
@@ -279,14 +266,14 @@ function restartQuiz() {
 	$('.scoreboard').html(scores());
 }
 
-// Renders the question form
-function renderQuestion() {
-	$('.box').html(generateQuestion());
+// Handles the display of the scores
+function scores() {
+	const scoreMake = `
+        <li class="scores">Correct: ${correctAnswers}</li>
+        <li class="scores">Incorrect: ${incorrectAnswers}</li>
+    `;
+	return scoreMake;
 }
 
-// First thing to run when the page loads
-function handlePage() {
-	buttons();
-}
 
-$(handlePage);
+
